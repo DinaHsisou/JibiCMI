01872ec47d3346f4ca854fd52a6f18a86e9b12e2
PC <dinahsisou@gmail.com>
2024-05-23 19:20:14 +0100

    test

--- src/main/java/com/example/JibiCMI/model/Client.java
+++ src/main/java/com/example/JibiCMI/model/Client.java
@@ -1,4 +1,24 @@
 package com.example.JibiCMI.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Builder
 public class Client {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+
+    @OneToOne(mappedBy = "client")
+    private Account account;
+
 }
