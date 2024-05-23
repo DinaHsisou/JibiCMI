206f0d29020fce4093620928d6f63b06c1506a88
Taha <tahabouataoui5@gmail.com>
2024-05-23 19:21:08 +0100

    tee

--- src/main/java/com/example/JibiCMI/model/Account.java
+++ src/main/java/com/example/JibiCMI/model/Account.java
@@ -1,4 +1,28 @@
 package com.example.JibiCMI.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Builder
+
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Float balance=0F;
+
+    @OneToOne
+    private Client client;;
+
+
 }
